f24d301cbcc3fd033ba64c6c2276a6ad15e3a501
Hysen Lubovci <hysen.lubovci@student.uni-pr.edu>
2023-05-15 12:57:01 -0700

    #

--- dbConfig.php
+++ dbConfig.php
@@ -1,14 +1,14 @@
 <?php  
-$dbHost     = "localhost";  
-$dbUsername = "root";  
-$dbPassword = "";  
-$dbName     = "staff";  
-  
-// Create database connection  
-$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);  
-  
-// Check connection  
-if ($db->connect_error) {  
-    die("Connection failed: " . $db->connect_error);  
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
 ?>
